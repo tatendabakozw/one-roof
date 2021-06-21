85a4204e34eb7e5a75c4cfb1f5742dd1718cc31c
tatendabakozw <tatendabakozw@gmail.com>
2021-06-21 23:10:08 +0200

    home navbar refactor

---
+++ helpers/nav_option.ts
@@ -0,0 +1,35 @@
+const nav_options ={
+    nav_options : [
+        {
+            option: "i'm new here?",
+            id: "1",
+            location: '/becomeaseller'
+        }
+    ],
+    mobile_nav_options : [
+        {
+            option: "Log In",
+            id: "1",
+            location: '/auth/login'
+        },
+        {
+            option: "Sign Up",
+            id: "1",
+            location: '/auth/login'
+        }
+    ],
+    left_options : [
+        {
+          option: "Sell",
+          id: "1",
+          location: '/sell'
+        },
+        {
+          option: "Buy",
+          id: "2",
+          location: '/buy'
+        }
+      ]
+}
+
+export default nav_options
